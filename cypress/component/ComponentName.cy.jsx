910b97608476d988fb79635fe83aa118bb4bf04a
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2024-04-30 11:22:14 +0200

    patch

--- cypress/component/ComponentName.cy.jsx
+++
@@ -1,8 +0,0 @@
-describe('ComponentName.cy.jsx', () => {
-  beforeEach(() => {
-    cy.viewport(1280, 720) // Définit le viewport à 1280x720
-  })
-  it('playground', () => {
-    // cy.mount()
-  })
-})
